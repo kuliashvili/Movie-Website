1ce83727f9c4d598497a83c4c0bc13f65e545a7c
yulo01 <giorgi.kuliashvili.1@btu.edu.ge>
2023-05-11 14:42:49 +0400

    movieList update items

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function MovieList(props) {
   return (
    <>
-   {props.movies.map((movie, index) => <div><img src={movie.Poster} alt="movie" /></div>)}
+   {props.movies.map((movie, index) => <div className='item'><img className='item-img' src={movie.Poster} alt="movie" /></div>)}
    </>
   )
 }
