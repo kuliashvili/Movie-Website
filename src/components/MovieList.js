7323f735ef451dac277279b4ff465821c6b0a181
yulo01 <giorgi.kuliashvili.1@btu.edu.ge>
2023-05-12 17:34:19 +0400

    movielist styles update

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,7 +3,11 @@ import React from 'react'
 export default function MovieList(props) {
   return (
    <>
-   {props.movies.map((movie, index) => <div className='item'><img className='item-img' src={movie.Poster} alt="movie" /></div>)}
+   {props.movies.map((movie, index) => 
+   <div className='item'>
+    <img className='item-img' src={movie.Poster} alt="movie" />
+    <div className='overlay'></div>
+    </div>)}
    </>
   )
 }
