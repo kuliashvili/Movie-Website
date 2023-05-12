1f8e92c2f91058099f6107c4e805c4b69079c0c7
yulo01 <giorgi.kuliashvili.1@btu.edu.ge>
2023-05-12 19:54:46 +0400

    add localStorage function

--- src/App.js
+++ src/App.js
@@ -31,14 +31,27 @@ function App() {
     getMovieRecuest(searchValue)
   }, [searchValue])
 
+  useEffect(() => {
+     const movieFavourites = JSON.parse(
+      localStorage.getItem('react-movie-app-favourites'))
+
+      setFavourites(movieFavourites)
+  }, [])
+
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem('react-movie-app-favourites', JSON.stringify(items))
+  }
+
   const addFavouriteMovie = (movie) => {
       const newFavouriteList = [...favourites, movie]
       setFavourites(newFavouriteList)
+      saveToLocalStorage(newFavouriteList)
   }
   const removeFavouriteMovie = (movie) => {
       const newFavouriteList = favourites.filter(
         (favourite) => favourite.imdbID !== movie.imdbID)
       setFavourites(newFavouriteList)
+      saveToLocalStorage(newFavouriteList)
   }
 
   
